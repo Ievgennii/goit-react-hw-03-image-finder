fd9e84891a64c3dccd37d646b1e6bac2ac57e00f
Ievgennii <simonovievgenii@gmail.com>
2023-05-11 18:02:03 +0300

    creat api

---
+++ src/components/Api.js
@@ -0,0 +1,14 @@
+// api.js
+
+import axios from "axios";
+
+export const getImagesWithQuery = async (search, page) => {
+  const KEY = '34611977-1c6ac37fcf885911789ad5cb9';
+
+  const response = await axios.get(
+    `/?q=${search}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  );
+
+  return response.data.hits;
+};
+
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,73 +2,117 @@ import React, { Component } from 'react';
 import ImageGalleryItem from './ImageGalleryItem';
 import Button from './Button';
 import { MagnifyingGlass } from 'react-loader-spinner';
+import { getImagesWithQuery } from './Api';
 import axios from 'axios';
 import css from './styles.module.css';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 class ImageGallery extends Component {
+
   state = {
     images: [],
     isLoading: false,
     error: null,
-    search: null,
-    emptyResponce: false,
     page: 1,
+    search: '',
+    emptyResponse: false,
   };
 
   async componentDidUpdate(prevProps, prevState) {
-    const KEY = '34611977-1c6ac37fcf885911789ad5cb9';
-
-
-    if (prevProps.search !== this.props.search) {  
+    if (prevProps.search !== this.props.search) {
       this.setState({ isLoading: true });
       this.setState({ search: this.props.search });
       this.setState({ page: 1 });
       this.setState({ error: null });
 
       try {
-        const response = await axios.get(
-          `/?q=${this.props.search}&page=${this.state.page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
-        );
+        const images = await getImagesWithQuery(this.props.search, 1);
         this.setState({
-          images: response.data.hits,
-          emptyResponce: !response.data.hits.length,
+          images: images,
+          emptyResponce: !images.length,
         });
       } catch (error) {
         this.setState({ error });
         alert(`Whoops, something went wrong: ${error.message}`);
       } finally {
         this.setState({ isLoading: false });
-        console.log(this.state.images);
       }
     }
 
     if (prevState.page !== this.state.page) {
-      
       this.setState({ isLoading: true });
       this.setState({ search: this.props.search });
-      
 
       try {
-        
-        const response = await axios.get(
-          `/?q=${this.props.search}&page=${this.state.page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
-        );
-        this.setState({
-          images: response.data.hits,
-          
-        });
+        const images = await getImagesWithQuery(this.props.search, this.state.page);
+        this.setState({ images });
       } catch (error) {
         this.setState({ error });
-        this.setState({ page: 1 });
+        // this.setState({ page: 1 });
         alert("We're sorry, but you've reached the end of search results.");
       } finally {
         this.setState({ isLoading: false });
-        
       }
     }
   }
+  
+  // state = {
+  //   images: [],
+  //   isLoading: false,
+  //   error: null,
+  //   search: null,
+  //   emptyResponce: false,
+  //   page: 1,
+  // };
+
+  // async componentDidUpdate(prevProps, prevState) {
+  //   const KEY = '34611977-1c6ac37fcf885911789ad5cb9';
+
+  //   if (prevProps.search !== this.props.search) {
+  //     this.setState({ isLoading: true });
+  //     this.setState({ search: this.props.search });
+  //     this.setState({ page: 1 });
+  //     this.setState({ error: null });
+
+  //     try {       
+
+  //       const response = await axios.get(
+  //         `/?q=${this.props.search}&page=${this.state.page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  //       );
+  //       this.setState({
+  //         images: response.data.hits,
+  //         emptyResponce: !response.data.hits.length,
+  //       });
+  //     } catch (error) {
+  //       this.setState({ error });
+  //       alert(`Whoops, something went wrong: ${error.message}`);
+  //     } finally {
+  //       this.setState({ isLoading: false });
+  //       console.log(this.state.images);
+  //     }
+  //   }
+
+  //   if (prevState.page !== this.state.page) {
+  //     this.setState({ isLoading: true });
+  //     this.setState({ search: this.props.search });
+
+  //     try {
+  //       const response = await axios.get(
+  //         `/?q=${this.props.search}&page=${this.state.page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  //       );
+  //       this.setState({
+  //         images: response.data.hits,
+  //       });
+  //     } catch (error) {
+  //       this.setState({ error });
+  //       this.setState({ page: 1 });
+  //       alert("We're sorry, but you've reached the end of search results.");
+  //     } finally {
+  //       this.setState({ isLoading: false });
+  //     }
+  //   }
+  // }
 
   changePage = () => {
     if (this.state.images.length === 12) {
@@ -84,7 +128,6 @@ class ImageGallery extends Component {
     return (
       <div>
         <ul className={css.ImageGallery}>
-          
           {isLoading && <MagnifyingGlass />}
           {emptyResponce && <p>Ничего не найдено</p>}
 
